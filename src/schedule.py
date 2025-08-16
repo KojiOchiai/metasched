8c11f882dce45ead8907189d4826e82b1aca2023
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2025-08-17 00:39:29 +0900

    debug: update to run add schedule from finished time

--- src/schedule.py
+++ src/schedule.py
@@ -10,14 +10,39 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-async def process_tasks_with_agent(await_list: AwaitList, agent: Agent):
+async def process_tasks_with_agent(
+    await_list: AwaitList, executor: Agent, scheduler: Agent
+):
+    scheduler_history: list = []
     async for task in await_list.wait_for_next_task():
         logger.info(
             f"[ProcessTask] Executing task: {task.content} at {task.execution_time}"
         )
-        result = await agent.run(task.content + "is finished")
-        print(result.output)
-        logger.info(f"[ProcessTask] Result for task {task.id}: {result.output}")
+        started_at = datetime.now()
+        result = await executor.run(
+            f"Start task: id={task.id}, scheduled_at={task.execution_time}, "
+            f"contents={task.content}"
+        )
+        finished_at = datetime.now()
+        logger.info(
+            f"[ProcessTask] Result for running task {task.id}: {result.output}"
+            f"started_at={started_at}, "
+            f"finished_at={finished_at}"
+        )
+        result = await scheduler.run(
+            (
+                f"Task finished: id={task.id}, "
+                f"scheduled_at={task.execution_time}, "
+                f"started_at={started_at}, "
+                f"finished_at={finished_at}, "
+                f"contents={task.content}, result={result.output}"
+            ),
+            message_history=scheduler_history,
+        )
+        scheduler_history = result.all_messages()
+        logger.info(
+            f"[ProcessTask] Result for after finishing task {task.id}: {result.output}"
+        )
 
 
 def get_add_task(await_list: AwaitList):
@@ -34,6 +59,18 @@ def get_add_task(await_list: AwaitList):
         return task
 
     return add_task
+
+
+def get_get_tasks(await_list: AwaitList):
+    async def get_tasks():
+        """
+        Get the list of all tasks.
+        """
+        tasks = await_list.get_tasks()
+        logger.info("[GetTasks] Get current tasks")
+        return tasks
+
+    return get_tasks
 
 
 def get_time() -> datetime:
