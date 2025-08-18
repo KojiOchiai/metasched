13bd400831fb7f50a386ebae840207d6feec4b15
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2025-08-18 19:05:27 +0900

    fix: rename settings to maholo_settings for clarity and consistency

--- src/settings.py
+++ src/settings.py
@@ -1,15 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
+class MaholoSettings(BaseSettings):
     host: str = "localhost"
     port: int = 63001
     base_path: str = "C:\\BioApl\\DataSet\\proteo-03\\Protocol\\"
     microscope_image_dir: str = "./nikon_save/"
 
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", env_prefix="METASHED_"
+        env_file=".env", env_file_encoding="utf-8", env_prefix="MAHOLO_"
     )
 
 
-settings = Settings()
+maholo_settings = MaholoSettings()
