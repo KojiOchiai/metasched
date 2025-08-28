3f50ca768745ad598061be89121b026c88e735e1
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2025-08-28 21:35:58 +0900

    feat: allow multiple subnodes by add

--- src/labmodel.py
+++ src/labmodel.py
@@ -7,8 +7,13 @@ from typing import Union
 class Node(ABC):
     name: str
 
+    def add(self, *others: "Node") -> "Node":
+        for other in others:
+            self._add(other)
+        return self
+
     @abstractmethod
-    def add(self, other):
+    def _add(self, other):
         raise NotImplementedError()
 
     @abstractmethod
@@ -20,7 +25,7 @@ class Node(ABC):
 class Liquid(Node):
     volume: float = 0.0  # in microliter
 
-    def add(self, other: "Liquid") -> "Liquid":
+    def _add(self, other: "Liquid") -> "Liquid":
         raise ValueError("Cannot add to Liquid")
 
     def __str__(self, indent=0):
@@ -32,7 +37,7 @@ class Well(Node):
     max_volume: float = 1000.0  # in microliter
     sub_node: list[Liquid] = field(default_factory=list)
 
-    def add(self, other: Liquid) -> "Well":
+    def _add(self, other: Liquid) -> "Well":
         if not isinstance(other, Liquid):
             raise ValueError("Can only add Liquid to Well")
         if sum(liq.volume for liq in self.sub_node) + other.volume > self.max_volume:
@@ -52,7 +57,7 @@ class Jig(Node):
     accept: str = "any"
     sub_node: list["Object"] = field(default_factory=list)
 
-    def add(self, other: "Object") -> "Jig":
+    def _add(self, other: "Object") -> "Jig":
         if not isinstance(other, Object):
             raise ValueError("Can only add Object to Jig")
         if self.accept != "any" and self.accept != other.shape:
@@ -72,7 +77,7 @@ class Object(Node):
     shape: str = field(default="unknown")
     sub_node: list[Union[Jig, Well]] = field(default_factory=list)
 
-    def add(self, other: Union[Jig, Well]) -> "Object":
+    def _add(self, other: Union[Jig, Well]) -> "Object":
         if not isinstance(other, (Jig, Well)):
             raise ValueError("Can only add Jig or Well to Object")
         self.sub_node.append(other)
@@ -89,7 +94,7 @@ class Object(Node):
 class Root(Node):
     sub_node: list[Object] = field(default_factory=list)
 
-    def add(self, other: Object) -> "Root":
+    def _add(self, other: Object) -> "Root":
         if not isinstance(other, Object):
             raise ValueError("Can only add Object to Root")
         self.sub_node.append(other)
@@ -104,24 +109,21 @@ class Root(Node):
 
 if __name__ == "__main__":
     root = Root(name="Root").add(
-        Object(name="TubeRack", shape="tube_rack")
-        .add(
+        Object(name="TubeRack", shape="tube_rack").add(
             Jig(name="Jig1", accept="50mlTube").add(
                 Object(name="50mlTube1", shape="50mlTube").add(
                     Well(name="1", max_volume=5000.0).add(
                         Liquid(name="Water", volume=100.0)
                     )
                 )
-            )
-        )
-        .add(
+            ),
             Jig(name="Jig2", accept="PCRPlate").add(
                 Object(name="PCRPlate1", shape="PCRPlate").add(
                     Well(name="1", max_volume=5000.0).add(
                         Liquid(name="Reagent", volume=100.0)
                     )
                 )
-            )
+            ),
         )
     )
     print(root)
