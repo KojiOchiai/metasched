76de7e7ac3e355a9eb2285e8bc902813f7b14f11
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2025-09-05 02:53:50 +0900

    feat: implement Node, Start, Delay, and Protocol classes with dataclass support

---
+++ src/optimizer.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+from ortools.sat.python import cp_model
+
+from src import protocol
+
+model = cp_model.CpModel()
+
+
+@dataclass
+class Node:
+    id: UUID
+    post_node: list["Node"]
+
+
+@dataclass
+class Start(Node):
+    start_time: int
+
+
+@dataclass
+class Delay(Node):
+    duration: int
+    from_type: protocol.FromType
+    offset: int
+
+
+@dataclass
+class Protocol(Node):
+    name: str
+    duration: int
+    wait: int
+    start: cp_model.IntVar | None = None
+    end: cp_model.IntVar | None = None
+    interval: cp_model.IntervalVar | None = None
