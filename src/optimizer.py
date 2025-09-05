dd7ac69dfb3363000d18de76d79f7521e38ee14b
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2025-09-05 18:32:20 +0900

    refactor: rename started_at and finished_at to started_time and finished_time in Protocol class

--- src/optimizer.py
+++ src/optimizer.py
@@ -30,13 +30,13 @@ class Delay(Node):
 class Protocol(Node):
     name: str
     duration: int
-    started_at: cp_model.IntVar | None = None
-    finished_at: cp_model.IntVar | None = None
+    started_time: cp_model.IntVar | None = None
+    finished_time: cp_model.IntVar | None = None
     interval: cp_model.IntervalVar | None = None
 
     def set_vars(self, model: cp_model.CpModel, max_time: int) -> None:
-        self.started_at = model.NewIntVar(0, max_time, f"{self.id}_started_at")
-        self.finished_at = model.NewIntVar(0, max_time, f"{self.id}_finished_at")
+        self.started_time = model.NewIntVar(0, max_time, f"{self.id}_started_time")
+        self.finished_time = model.NewIntVar(0, max_time, f"{self.id}_finished_time")
         self.interval = model.NewIntervalVar(
-            self.started_at, self.duration, self.finished_at, f"{self.id}_interval"
+            self.started_time, self.duration, self.finished_time, f"{self.id}_interval"
         )
