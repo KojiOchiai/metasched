8ad5e6f76a67ef381d8a8698656d5f35fd465663
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2025-09-05 18:32:31 +0900

    feat: add get_oldest_time and sum_durations functions, and TimeSecondsConverter class for time management

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime, timedelta
 from uuid import UUID
 
 from ortools.sat.python import cp_model
@@ -40,3 +41,69 @@ class Protocol(Node):
         self.interval = model.NewIntervalVar(
             self.started_time, self.duration, self.finished_time, f"{self.id}_interval"
         )
+
+
+def get_oldest_time(
+    protocols: list[protocol.Protocol | protocol.Delay | protocol.Start],
+) -> datetime:
+    min_time = min(
+        (
+            p.started_time.timestamp()
+            for p in protocols
+            if isinstance(p, protocol.Protocol) and p.started_time is not None
+        ),
+        default=0,
+    )
+    if min_time == 0:
+        return datetime.now()
+    return datetime.fromtimestamp(min_time)
+
+
+def sum_durations(
+    protocols: list[protocol.Protocol | protocol.Delay | protocol.Start],
+) -> float:
+    return sum(
+        (
+            p.duration.total_seconds()
+            for p in protocols
+            if isinstance(p, (protocol.Protocol | protocol.Delay))
+        ),
+        start=0,
+    )
+
+
+class TimeSecondsConverter:
+    def __init__(self, start_time: datetime):
+        self.start_time = start_time
+
+    def time_to_seconds(self, time: datetime) -> float:
+        return (time - self.start_time).total_seconds()
+
+    def seconds_to_time(self, seconds: float) -> datetime:
+        return self.start_time + timedelta(seconds=seconds)
+
+
+if __name__ == "__main__":
+    s = protocol.Start()
+    p1 = protocol.Protocol(name="P1", duration=timedelta(minutes=10))
+    p2 = protocol.Protocol(name="P2")
+    p3 = protocol.Protocol(name="P3")
+
+    sec5 = protocol.Delay(
+        duration=timedelta(seconds=5), from_type=protocol.FromType.START
+    )
+
+    s > p1 > p2
+    p1 > sec5 > p3
+
+    p1.started_time = datetime.now()
+    p2.started_time = datetime.now() + timedelta(minutes=15)
+    print(s)
+    oldest_time = get_oldest_time(s.flatten())
+    print("oldest time: ", oldest_time)
+    duration = sum_durations(s.flatten())
+    print("total duration: ", duration)
+    tsc = TimeSecondsConverter(oldest_time)
+    time_in_seconds = tsc.time_to_seconds(p2.started_time)
+    print("time in seconds: ", time_in_seconds)
+    print("time: ", tsc.seconds_to_time(time_in_seconds))
